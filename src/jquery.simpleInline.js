7b3581c133a8be51257b9ef169a2e0dc6aff3e63
Hendrik Beneke <hendrik.beneke@gmail.com>
2013-08-02 22:36:17 +0200

    improved multi line edit behavior

--- src/jquery.simpleInline.js
+++ src/jquery.simpleInline.js
@@ -2,22 +2,44 @@
 (function($, window, document, undefined) {
   "use strict";
 
+  var NEW_LINE = "\n";
+
   var pluginName = "simpleInline",
     defaults = {
       allowLineBreaks: false,
-      valueChanged: function(newValue){ console.log(newValue);}
+      valueChanged: function(newValue){ 
+        console.log(newValue);
+      }
     };
 
   function Plugin(element, options) {
     this.element = (element instanceof $) ? element : $(element);
-    this.settings = $.extend({}, defaults, options);
+    var inlineOptions = getDataAttributes(element);
+    this.settings = $.extend({}, defaults, inlineOptions, options);
     this._defaults = defaults;
     this._name = pluginName;
     this.init();
   }
 
+  function getDataAttributes(element){
+    var data = $(element).data(),
+      inlineOptions = {};
+    for(var key in data) {
+      var cKey = optionToCamelCase(key);
+      inlineOptions[cKey] = data[key];
+    }
+    return inlineOptions;
+  }
+
+  function optionToCamelCase(option){
+    return option.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase() });
+  }
+
   Plugin.prototype = {
     init: function() {
+      if(this.settings.allowLineBreaks){
+        this.convertBrToLf(this.element);
+      }
       this.setDefaultEvents();
       this.setPlaceholderAttribute();
       this.setPlaceholderAction();
@@ -25,22 +47,25 @@
 
     setDefaultEvents: function(){
       var _this = this;
+      var editingClass = _this.settings.allowLineBreaks ? 'editing-block' : 'editing'
       this.element.on('click.editable', function(){
-        $(this).addClass('editing');
+        $(this).addClass(editingClass);
         $(this).attr("contenteditable", true);
+        $(this).focus();
       });
       this.element.on('blur.editable', function(){
-        $(this).removeClass('editing');
+        $(this).removeClass(editingClass);
         $(this).attr("contenteditable", false);
+        _this.clearContentIfEmpty(this);
         _this.settings.valueChanged.call(_this, _this.getValue());
       });
 
-      this.element.keydown(function(e) {
-        if (e.keyCode === "13") {
-          if (e.shiftKey && _this.settings.allowLineBreaks) {
-            // pasteIntoInput(this, "\n");
+      this.element.on('keydown.editable', function(e) {
+        if (e.keyCode === 13) {
+          e.preventDefault();
+          if (_this.settings.allowLineBreaks) {
+            _this.pasteIntoInput(NEW_LINE);    
           } else {
-            e.preventDefault();
             $(this).blur();
           }
         }
@@ -57,7 +82,7 @@
       var $el = this.element,
         hasPlaceholder = this.hasPlaceholder;
       if (this.hasPlaceholder) {
-        if (!$el.html()) {
+        if (!$el.text().trim()) {
           $el.addClass("inline-placeholder");
         }
         $el.focusin(function() {
@@ -65,7 +90,7 @@
             $el.removeClass("inline-placeholder");
           }
         }).focusout(function() {
-          if (hasPlaceholder && !$el.html()) {
+          if (hasPlaceholder && !$el.text().trim()) {
             $el.addClass("inline-placeholder");
           }
         });
@@ -77,9 +102,22 @@
     },
 
     getValue: function(){
-      return this.settings.allowLineBreaks ? this.element.html() : this.element.text();
+      return this.settings.allowLineBreaks ? this.element.text() : this.element.text();
     },
 
+    convertBrToLf: function(el){
+      el.html(el.html().replace(/<br\s*[\/]?>/gi, NEW_LINE));
+    },
+
+    pasteIntoInput: function(html) {
+      document.execCommand("insertHTML", false, html);
+    },
+
+    clearContentIfEmpty: function(el){
+      if ($(el).text().trim() == "") {
+        $(el).empty(); // chrome and FF add an extra <br> if the div is empty. we dont want that
+      };
+    }
   };
 
   $.fn[pluginName] = function(options) {
